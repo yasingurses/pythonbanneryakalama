da36d627a530741b23c3360191f9ab4ec9cdef9f
yasingurses <term213@hotmail.com>
2020-09-10 14:09:03 -0400

    pythonbanneryakalama

---
+++ banneryakala.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+os.system("apt-get install figlet -y")
+os.system("apt-get install nc -y")
+os.system("apt-get install nmap -y")
+os.system("clear")
+os.system("figlet BANNER YAKALA")
+print("""
+Bu Araçla Servislerin İlk Açılış(Hoş Geldiniz) Ekranındaki Bilgileri Yakalamak İçin Kullanılır.
+
+1) Tek Servis Banner
+2) Tüm Servis Ban
